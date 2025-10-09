98b5af49296c5593d0521bfc39be2d4f56c4bb32
Manichandra <manichandra.vallala@gmail.com>
2025-10-09 23:29:09 +0530

    script

---
+++ NEW_Welding/Assets/Scripts/MIG_Torch.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MIG_Torch : MonoBehaviour
+{
+    public GameObject lens;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("torch"))
+        {
+            lens.SetActive(true);
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("torch"))
+        {
+            lens.SetActive(false);
+        }
+    }
+}
